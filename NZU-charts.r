74f4c7ad4a0e8ff52f613754fd3494ecd9e36062
theecanmole <theecanmole@gmail.com>
2016-12-26 17:51:18 +1300

    update data, chart

--- NZU-charts.r
+++ NZU-charts.r
@@ -1,10 +1,19 @@
-# Create charts of processed monthly mean NZU prices 
+# Create charts of processed monthly mean NZU prices
 
-svg(filename="NZUprice-720by540-11-16.svg", width = 8, height = 6, pointsize = 16, onefile = FALSE, family = "sans", bg = "white", antialias = c("default", "none", "gray", "subpixel"))  
+# download monthly price data from Github
+
+urldata <- c("https://raw.githubusercontent.com/theecanmole/nzu/master/nzu-month-price-2010-2016.csv")
+                 
+monthprice <-c("/home/simon/R/nzu/nzu-month-price-2010-2016.csv")
+
+download.file(urldata, monthprice)
+
+svg(filename="NZUprice-720by540.svg", width = 8, height = 6, pointsize = 16, onefile = FALSE, family = "sans", bg = "white", antialias = c("default", "none", "gray", "subpixel"))  
 par(mar=c(2.7,2.7,1,1)+0.1)
-plot(monthprice,ylim=c(0,24),tck=0.01,axes=TRUE,ann=TRUE, las=1,col=2,lwd=2,type='l',lty=1)
+plot(monthprice[["decimal"]],monthprice[["price"]],ylim=c(0,24),tck=0.01,axes=TRUE,ann=TRUE, las=1,col=2,lwd=2,type='l',lty=1)
+grid(col="darkgray",lwd=1)
 axis(side=4, tck=0.01, las=0,tick=TRUE,labels = FALSE)
-mtext(side=1,cex=1,line=-1.3,"Data: 'NZU-prices-data-2010-2016' http://bit.ly/2fHbojr")
+mtext(side=1,cex=1,line=-1.3,"Data: 'NZU monthly prices' https://github.com/theecanmole/nzu")
 mtext(side=3,cex=1.7, line=-2.2,expression(paste("New Zealand Unit Prices 2010 - 2016")) )
 mtext(side=2,cex=1, line=-1.3,"$NZ Dollars/tonne")
 mtext(side=4,cex=0.75, line=0.05,R.version.string)
@@ -12,8 +21,9 @@ dev.off()
 
 png("NZU-800by600-2016-11.png", bg="white", width=800, height=600,pointsize = 16)
 par(mar=c(2.7,2.7,1,1)+0.1)
-plot(monthprice,ylim=c(0,24),tck=0.01,axes=TRUE,ann=TRUE,las=1,col="#F32424",lwd=2,type='l')
-mtext(side=1,cex=1,line=-1.2,"Data: 'NZU-price-data-2010-2016' http://bit.ly/2fHbojr")
+plot(monthprice[["decimal"]],monthprice[["price"]],ylim=c(0,24),tck=0.01,axes=TRUE,ann=TRUE,las=1,col="#F32424",lwd=2,type='l')
+grid(col="darkgray",lwd=1)
+mtext(side=1,cex=1,line=-1.2,"Data: 'NZU monthly prices' https://github.com/theecanmole/nzu")
 mtext(side=3,cex=2, line=-2.5,expression(paste("New Zealand Unit Prices 2010 - 2016")) )
 mtext(side=2,cex=1, line=-1.3,"$NZ Dollars/tonne")
 mtext(side=4,cex=0.75, line=0.05,R.version.string)
@@ -22,8 +32,8 @@ dev.off()
 # Use 'decimal' year vector in place of date-formatted month vector
 svg(filename="NZUpricedecimal-720by540-11-2016.svg", width = 8, height = 6, pointsize = 16, onefile = FALSE, family = "sans", bg = "white", antialias = c("default", "none", "gray", "subpixel"))  
 par(mar=c(2.7,2.7,1,1)+0.1)
-plot(decimal,monthprice[["price"]],ylim=c(0,24),tck=0.01,axes=TRUE,ann=TRUE, las=1,col=2,lwd=1,type='l',lty=1)
-points(decimal,monthprice[["price"]],pch = 20)
+plot(monthprice[["decimal"]],monthprice[["price"]],ylim=c(0,24),tck=0.01,axes=TRUE,ann=TRUE, las=1,col=2,lwd=1,type='l',lty=1)
+points(monthprice[["decimal"]],monthprice[["price"]],pch = 20)
 grid(col="darkgray",lwd=1)
 axis(side=4, tck=0.01, las=0,tick=TRUE,labels = FALSE)
 mtext(side=1,cex=1,line=-1.3,"Data: 'NZU-prices-data-2010-2016' http://bit.ly/2fHbojr")
--- NZU-monthly-mean-2010-2016.r
+++ NZU-monthly-mean-2010-2016.r
@@ -10,7 +10,7 @@ rawdata <-c("/home/simon/R/nzu/nzu-weekly-prices-data-2010-2016.csv")
 
 download.file(urlrawdata, rawdata)
 
-# read in raw prices data
+# or read in raw prices data from a local folder
 
 rawdata <- read.csv("/home/simon/R/nzu/nzu-weekly-prices-data-2010-2016.csv", skip=0, header=TRUE, sep=",", colClasses = c("Date","numeric","character"),na.strings="NA", dec=".", strip.white=TRUE)
 
@@ -19,21 +19,21 @@ rawdata <- read.csv("/home/simon/R/nzu/nzu-weekly-prices-data-2010-2016.csv", sk
 # examine dataframe
 
 str(rawdata)
-'data.frame':	384 obs. of  3 variables:
+'data.frame':	391 obs. of  3 variables:
  $ date     : Date, format: "2010-05-14" "2010-05-21" ...
  $ price    : num  17.8 17.5 17.5 17 17.8 ...
  $ reference: chr  "http://www.carbonnews.co.nz/story.asp?storyID=4529" "http://www.carbonnews.co.nz/story.asp?storyID=4540" "http://www.carbonnews.co.nz/story.asp?storyID=4540" "http://www.carbonnews.co.nz/story.asp?storyID=4588" ...
-
+ 
 # add month variable/factor to data
 rawdata$month <- as.factor(format(rawdata$date, "%Y-%m"))
 
 # examine dataframe
 str(rawdata)
-'data.frame':	373 obs. of  4 variables:
+'data.frame':	391 obs. of  4 variables:
  $ date     : Date, format: "2010-05-14" "2010-05-21" ...
  $ price    : num  17.8 17.5 17.5 17 17.8 ...
  $ reference: chr  "http://www.carbonnews.co.nz/story.asp?storyID=4529" "http://www.carbonnews.co.nz/story.asp?storyID=4540" "http://www.carbonnews.co.nz/story.asp?storyID=4540" "http://www.carbonnews.co.nz/story.asp?storyID=4588" ...
- $ month    : Factor w/ 79 levels "2010-05","2010-06",..: 1 1 1 2 2 2 3 3 4 4 ...
+ $ month    : Factor w/ 80 levels "2010-05","2010-06",..: 1 1 1 2 2 2 3 3 4 4 ...
 
 # create new dataframe of monthly mean price 
 monthprice<-aggregate(price ~ month, rawdata, mean)
@@ -48,19 +48,21 @@ str(monthprice)
 
 # replace month factor with mid month 15th of month date formatted object 
 monthprice[["month"]] = seq(as.Date('2010-05-15'), by = 'months', length = nrow(monthprice)) 
+
 # round mean prices to whole cents
 monthprice[["price"]] = round(monthprice[["price"]], digits = 2)
 
-# create 'decimal' year vector as an alternative to the month vector, e.g. May 2010 = 2010.333, Jan 2011 = 2011.000
-decimal = seq(2010.333, by = 1/12, length = nrow(monthprice)) 
+# create 'decimal' year vector as an alternative to the month vector, e.g. May 2010 = 2010.375, Jan 2011 = 2011.04167
+monthprice[["decimal"]] = seq(2010.375, by = 1/12, length = nrow(monthprice)) 
 
-# examine dataframe
+# examine dataframe - again
 str(monthprice)
-'data.frame':	80 obs. of  2 variables:
- $ month: Date, format: "2010-05-15" "2010-06-15" ...
- $ price: num  17.6 17.4 18.1 18.4 20.2 ...
+'data.frame':	80 obs. of  3 variables:
+ $ month  : Date, format: "2010-05-15" "2010-06-15" ...
+ $ price  : num  17.6 17.4 18.1 18.4 20.2 ...
+ $ decimal: num  2010 2010 2011 2011 2011 ...
 
-# write new monthly data to a .csv file 
+# write the new monthly data to a .csv file 
 
 write.table(monthprice, file = "/home/simon/R/nzu/nzu-month-price-2010-2016.csv", sep = ",", col.names = TRUE, qmethod = "double",row.names = FALSE)
 
