d2d7b2bfca4e057728e904288ad32f4072ec89cb
theecanmole <theecanmole@gmail.com>
2019-02-18 18:59:06 +1300

    edit Reademe

--- NZU-charts.r
+++ NZU-charts.r
@@ -21,7 +21,7 @@ monthprice <- read.csv("/home/simon/nzu-month-price.csv", skip=0, header=TRUE, s
 
 # examine dataframe
 str(monthprice)
-'data.frame':	104 obs. of  3 variables:
+'data.frame':	106 obs. of  3 variables:
  $ month  : Date, format: "2010-05-15" "2010-06-15" ...
  $ price  : num  17.6 17.4 18.1 18.4 20.2 ...
  $ decimal: num  2010 2010 2011 2011 2011 ...
@@ -38,7 +38,7 @@ abline(h=c(5,10,15,20,25),col="lightgray",lwd=0.5,lty=2)
 axis(side=4, tck=0.01, las=0,tick=TRUE,labels = FALSE)
 axis(side=2, tck=0.01, las=0,tick=TRUE,labels = TRUE)
 mtext(side=1,cex=0.75,line=-1.3,"Data: 'NZU monthly prices' https://github.com/theecanmole/nzu")
-mtext(side=3,cex=1.2, line=-2.2,expression(paste("New Zealand Unit Prices 2010 - 2018")) )
+mtext(side=3,cex=1.2, line=-2.2,expression(paste("New Zealand Unit Prices 2010 - 2019")) )
 mtext(side=2,cex=0.75, line=-1.3,"$NZ Dollars/tonne")
 mtext(side=4,cex=0.75, line=0.05,R.version.string)
 dev.off()
@@ -50,7 +50,7 @@ plot(monthprice[["decimal"]],monthprice[["price"]],ylim=c(0,25),tck=0.01,axes=TR
 grid(col="darkgray",lwd=1)
 axis(side=4, tck=0.01, las=0,tick=TRUE,labels = FALSE)
 mtext(side=1,cex=1,line=-1.3,"Data: 'NZU monthly prices' https://github.com/theecanmole/nzu")
-mtext(side=3,cex=1.7, line=-2.2,expression(paste("New Zealand Unit Prices 2010 - 2018")) )
+mtext(side=3,cex=1.7, line=-2.2,expression(paste("New Zealand Unit Prices 2010 - 2019")) )
 mtext(side=2,cex=1, line=-1.3,"$NZ Dollars/tonne")
 mtext(side=4,cex=0.75, line=0.05,R.version.string)
 dev.off()
