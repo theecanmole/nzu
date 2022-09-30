db393a1f16787e82749e8a3456f874dd29eec6e1
theecanmole <theecanmole@gmail.com>
2022-09-30 22:13:44 +1300

    update

--- NZU-charts.r
+++ NZU-charts.r
@@ -21,7 +21,7 @@ monthprice <- read.csv("nzu-month-price.csv", skip=0, header=TRUE, sep=",", colC
 
 # examine dataframe
 str(monthprice)
-'data.frame':	148 obs. of  3 variables:
+'data.frame':	149 obs. of  3 variables:
  $ month  : Date, format: "2010-05-15" "2010-06-15" ...
  $ price  : num  17.6 17.4 18.1 18.4 20.2 ...
  $ decimal: num  2010 2010 2011 2011 2011 ...
@@ -77,8 +77,8 @@ par(mar=c(2.7,2.7,1,1)+0.1)
 plot(rawdata[["date"]],rawdata[["price"]],tck=0.01,axes=TRUE,ann=TRUE, las=1,col=2,lwd=2,type='l',lty=1)
 grid(col="darkgray",lwd=1)
 axis(side=4, tck=0.01, las=0,tick=TRUE,labels = FALSE)
-mtext(side=1,cex=1,line=-1.3,"Data: 'NZU weekly prices' https://github.com/theecanmole/nzu")
-mtext(side=3,cex=1.3, line=-2.2,expression(paste("New Zealand Unit Prices 2010 - 2022")) )
+mtext(side=1,cex=0.9,line=-1.3,"Data: 'NZU weekly prices' https://github.com/theecanmole/nzu")
+mtext(side=3,cex=1.3, line=-2.2,expression(paste("New Zealand Unit Spot Prices 2010 - 2022")) )
 mtext(side=2,cex=1, line=-1.3,"$NZ Dollars/tonne")
 mtext(side=4,cex=0.75, line=0.05,R.version.string)
 dev.off()
