de0b0d7af473c132a9c14ad30c53a6455f9acada
theecanmole <theecanmole@gmail.com>
2022-12-19 19:24:37 +1300

    update

--- NZU-monthly-mean.r
+++ NZU-monthly-mean.r
@@ -22,7 +22,7 @@ rawdata <- read.csv("nzu-weekly-prices-data.csv", skip=0, header=TRUE, sep=",", 
 # examine dataframe
 
 str(rawdata)
-'data.frame':	1486 obs. of  3 variables:
+'data.frame':	1516 obs. of  3 variables:
  $ date     : Date, format: "2010-05-14" "2010-05-21" ...
  $ price    : num  17.8 17.5 17.5 17 17.8 ...
  $ reference: chr  "http://www.carbonnews.co.nz/story.asp?storyID=4529" "http://www.carbonnews.co.nz/story.asp?storyID=4540" "http://www.carbonnews.co.nz/story.asp?storyID=4540" "http://www.carbonnews.co.nz/story.asp?storyID=4588" ...
@@ -32,25 +32,25 @@ rawdata$month <- as.factor(format(rawdata$date, "%Y-%m"))
 
 # examine dataframe
 str(rawdata)
-'data.frame':	1486 obs. of  4 variables:
+'data.frame':	1516 obs. of  4 variables:
  $ date     : Date, format: "2010-05-14" "2010-05-21" ...
  $ price    : num  17.8 17.5 17.5 17 17.8 ...
  $ reference: chr  "http://www.carbonnews.co.nz/story.asp?storyID=4529" "http://www.carbonnews.co.nz/story.asp?storyID=4540" "http://www.carbonnews.co.nz/story.asp?storyID=4540" "http://www.carbonnews.co.nz/story.asp?storyID=4588" ...
- $ month    : Factor w/ 150 levels "2010-05","2010-06",..: 1 1 1 2 2 2 3 3 4 4 ...
+ $ month    : Factor w/ 152 levels "2010-05","2010-06",..: 1 1 1 2 2 2 3 3 4 4 ...
 
 # create new dataframe of monthly mean price 
 monthprice<-aggregate(price ~ month, rawdata, mean)
 
 # examine dataframe
 str(monthprice)
-'data.frame':	151 obs. of  2 variables:
+'data.frame':	152 obs. of  2 variables:
  $ month: Factor w/ 149 levels "2010-05","2010-06",..: 1 2 3 4 5 6 7 8 9 10 ...
  $ price: num  17.6 17.4 18.1 18.4 20.2 ...
  
 # create a vector that is the number of months and the number of rows in 'monthprice' 
 lengthmonthprice <- length(monthprice[["month"]])
 lengthmonthprice
-[1] 151
+[1] 152
 
 # replace month factor with mid-month 15th of month date-formatted object 
 monthprice[["month"]] = seq(as.Date('2010-05-15'), by = 'months', length = nrow(monthprice)) 
@@ -63,7 +63,7 @@ monthprice[["decimal"]] = seq(2010.375, by = 1/12, length = nrow(monthprice))
 
 # examine dataframe - again
 str(monthprice)
-'data.frame':	151 obs. of  3 variables:
+'data.frame':	152 obs. of  3 variables:
  $ month  : Date, format: "2010-05-15" "2010-06-15" ...
  $ price  : num  17.6 17.4 18.1 18.4 20.2 ...
  $ decimal: num  2010 2010 2011 2011 2011 ...
@@ -83,7 +83,7 @@ rawdata$week <- as.factor(format(rawdata$date, "%Y-%m-%U"))
 weeklymeanprice <- aggregate(price ~ week, rawdata, mean) 
 
 str(weeklymeanprice)
-'data.frame':	600 obs. of  2 variables:
+'data.frame':	609 obs. of  2 variables:
  $ week : Factor w/ 600 levels "2010-05-19","2010-05-20",..: 1 2 3 4 5 6 7 8 9 10 ...
  $ price: num  17.8 17.5 17.5 17 17.8 ...  
 
@@ -100,10 +100,14 @@ head(weeklymeanprice,2)
 2 2010-05-20 17.50 2010-05-26
 
 str(weeklymeanprice)
-'data.frame':	602 obs. of  3 variables:
+'data.frame':	609 obs. of  3 variables:
  $ week     : Factor w/ 600 levels "2010-05-19","2010-05-20",..: 1 2 3 4 5 6 7 8 9 10 ...
  $ price    : num  17.8 17.5 17.5 17 17.8 ...
  $ weekended: Date, format: "2010-05-19" "2010-05-26" ...
+ tail(weeklymeanprice,2)
+          week  price  weekended
+608 2022-12-49 81.800 2022-01-05
+609 2022-12-50 81.624 2022-01-12 
 
 svg(filename="NZU-weekly-mean-price-720by540.svg", width = 8, height = 6, pointsize = 14, onefile = FALSE, family = "sans", bg = "white", antialias = c("default", "none", "gray", "subpixel"))  
 par(mar=c(2.7,2.7,1,1)+0.1)
