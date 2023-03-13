89a711971b9159deddbb80eaa8cf30f198aaab9b
theecanmole <theecanmole@gmail.com>
2023-03-13 19:11:25 +1300

    update

--- NZU-monthly-mean.r
+++ NZU-monthly-mean.r
@@ -23,7 +23,7 @@ rawdata <- read.csv("nzu-weekly-prices-data.csv", skip=0, header=TRUE, sep=",", 
 # examine dataframe
 
 str(rawdata)
-'data.frame':	1527 obs. of  3 variables:
+'data.frame':	1554 obs. of  3 variables:
  $ date     : Date, format: "2010-05-14" "2010-05-21" ...
  $ price    : num  17.8 17.5 17.5 17 17.8 ...
  $ reference: chr  "http://www.carbonnews.co.nz/story.asp?storyID=4529" "http://www.carbonnews.co.nz/story.asp?storyID=4540" "http://www.carbonnews.co.nz/story.asp?storyID=4540" "http://www.carbonnews.co.nz/story.asp?storyID=4588" ...
@@ -33,7 +33,7 @@ rawdata$month <- as.factor(format(rawdata$date, "%Y-%m"))
 
 # examine dataframe
 str(rawdata)
-'data.frame':	1527 obs. of  4 variables:
+'data.frame':	1554 obs. of  4 variables:
  $ date     : Date, format: "2010-05-14" "2010-05-21" ...
  $ price    : num  17.8 17.5 17.5 17 17.8 ...
  $ reference: chr  "http://www.carbonnews.co.nz/story.asp?storyID=4529" "http://www.carbonnews.co.nz/story.asp?storyID=4540" "http://www.carbonnews.co.nz/story.asp?storyID=4540" "http://www.carbonnews.co.nz/story.asp?storyID=4588" ...
@@ -44,14 +44,14 @@ monthprice<-aggregate(price ~ month, rawdata, mean)
 
 # examine dataframe
 str(monthprice)
-'data.frame':	153 obs. of  2 variables:
+'data.frame':	155 obs. of  2 variables:
  $ month: Factor w/ 152 levels "2010-05","2010-06",..: 1 2 3 4 5 6 7 8 9 10 ...
  $ price: num  17.6 17.4 18.1 18.4 20.2 ...
  
 # create a vector that is the number of months and the number of rows in 'monthprice' 
 lengthmonthprice <- length(monthprice[["month"]])
 lengthmonthprice
-[1] 153
+[1] 155
 
 # replace month factor with mid-month 15th of month date-formatted object 
 monthprice[["month"]] = seq(as.Date('2010-05-15'), by = 'months', length = nrow(monthprice)) 
@@ -64,7 +64,7 @@ monthprice[["decimal"]] = seq(2010.375, by = 1/12, length = nrow(monthprice))
 
 # examine dataframe - again
 str(monthprice)
-'data.frame':	153 obs. of  3 variables:
+'data.frame':	155 obs. of  3 variables:
  $ month  : Date, format: "2010-05-15" "2010-06-15" ...
  $ price  : num  17.6 17.4 18.1 18.4 20.2 ...
  $ decimal: num  2010 2010 2011 2011 2011 ...
@@ -76,29 +76,34 @@ rawdata$week
 rawdata$week <- trunc(rawdata$week) 
 
 str(rawdata)
-'data.frame':	1527 obs. of  5 variables:
+'data.frame':	1554 obs. of  5 variables:
  $ date     : Date, format: "2010-05-14" "2010-05-21" ...
  $ price    : num  17.8 17.5 17.5 17 17.8 ...
  $ reference: chr  "http://www.carbonnews.co.nz/story.asp?storyID=4529" "http://www.carbonnews.co.nz/story.asp?storyID=4540" "http://www.carbonnews.co.nz/story.asp?storyID=4540" "http://www.carbonnews.co.nz/story.asp?storyID=4588" ...
- $ month    : Factor w/ 153 levels "2010-05","2010-06",..: 1 1 1 2 2 2 3 3 4 4 ...
- $ week     :Class 'aweek'  atomic [1:1527] 2010-W19-5 2010-W20-5 2010-W21-6 2010-W23-5 ...
+ $ month    : Factor w/ 155 levels "2010-05","2010-06",..: 1 1 1 2 2 2 3 3 4 4 ...
+ $ week     :Class 'aweek'  atomic [1:1554] 2010-W19-5 2010-W20-5 2010-W21-6 2010-W23-5 ...
   .. ..- attr(*, "week_start")= int 1 
   
-# create new dataframe of weekly mean price using 'aweek' value 
+# create new dataframe of weekly mean price using 'aweek' variable 
 weeklyprice <- aggregate(price ~ week, rawdata, mean)
 
 # round mean prices to whole cents
 weeklyprice[["price"]] = round(weeklyprice[["price"]], digits = 2)
 
 str(weeklyprice)
-'data.frame':	570 obs. of  2 variables:
+'data.frame':	575 obs. of  2 variables:
  $ week :Class 'aweek'  atomic [1:570] 2010-W19 2010-W20 2010-W21 2010-W23 ...
   .. ..- attr(*, "week_start")= int 1
  $ price: num  17.8 17.5 17.5 17 17.8 ...  
 
 # convert and add aweek week to date format date 
 weeklyprice[["date"]] <- as.Date(weeklyprice[["week"]])
-  
+str(weeklyprice)
+'data.frame':	575 obs. of  3 variables:
+ $ week :Class 'aweek'  atomic [1:575] 2010-W19 2010-W20 2010-W21 2010-W23 ...
+  .. ..- attr(*, "week_start")= int 1
+ $ price: num  17.8 17.5 17.5 17 17.8 ...
+ $ date : Date, format: "2010-05-10" "2010-05-17" ...  
   
 # write the mean monthly price dataframe to a .csv file 
 
