eed118659948fa975c2f133331626a76010faea9
theecanmole <theecanmole@gmail.com>
2023-05-10 19:34:06 +1200

    update

--- NZU-monthly-mean.r
+++ NZU-monthly-mean.r
@@ -2,9 +2,6 @@ NZU-monthly-mean  R code
 
 # R code to create a mean monthly time series vector of prices of the New Zealand Emission Unit (NZU) from raw data of irregular prices webscraped from various web sources. 
 
-# locate and anchor the location of the working folder
-library(here)
-set_here()
 # load program aweek
 library(aweek)
 getwd()
@@ -23,7 +20,7 @@ rawdata <- read.csv("nzu-weekly-prices-data.csv", skip=0, header=TRUE, sep=",", 
 # examine dataframe
 
 str(rawdata)
-'data.frame':	1569 obs. of  3 variables:
+'data.frame':	1593 obs. of  3 variables:
  $ date     : Date, format: "2010-05-14" "2010-05-21" ...
  $ price    : num  17.8 17.5 17.5 17 17.8 ...
  $ reference: chr  "http://www.carbonnews.co.nz/story.asp?storyID=4529" "http://www.carbonnews.co.nz/story.asp?storyID=4540" "http://www.carbonnews.co.nz/story.asp?storyID=4540" "http://www.carbonnews.co.nz/story.asp?storyID=4588" ...
@@ -33,25 +30,25 @@ rawdata$month <- as.factor(format(rawdata$date, "%Y-%m"))
 
 # examine dataframe
 str(rawdata)
-'data.frame':	1569 obs. of  4 variables:
+'data.frame':	1593 obs. of  4 variables:
  $ date     : Date, format: "2010-05-14" "2010-05-21" ...
  $ price    : num  17.8 17.5 17.5 17 17.8 ...
  $ reference: chr  "http://www.carbonnews.co.nz/story.asp?storyID=4529" "http://www.carbonnews.co.nz/story.asp?storyID=4540" "http://www.carbonnews.co.nz/story.asp?storyID=4540" "http://www.carbonnews.co.nz/story.asp?storyID=4588" ...
- $ month    : Factor w/ 155 levels "2010-05","2010-06",..: 1 1 1 2 2 2 3 3 4 4 ...
+ $ month    : Factor w/ 157 levels "2010-05","2010-06",..: 1 1 1 2 2 2 3 3 4 4 ...
 
 # create new dataframe of monthly mean price 
 monthprice<-aggregate(price ~ month, rawdata, mean)
 
 # examine dataframe
 str(monthprice)
-'data.frame':	155 obs. of  2 variables:
- $ month: Factor w/ 155 levels "2010-05","2010-06",..: 1 2 3 4 5 6 7 8 9 10 ...
+'data.frame':	157 obs. of  2 variables:
+ $ month: Factor w/ 157 levels "2010-05","2010-06",..: 1 2 3 4 5 6 7 8 9 10 ...
  $ price: num  17.6 17.4 18.1 18.4 20.2 ...
  
 # create a vector that is the number of months and the number of rows in 'monthprice' 
 lengthmonthprice <- length(monthprice[["month"]])
 lengthmonthprice
-[1] 155
+[1] 157
 
 # replace month factor with mid-month 15th of month date-formatted object 
 monthprice[["month"]] = seq(as.Date('2010-05-15'), by = 'months', length = nrow(monthprice)) 
@@ -64,7 +61,7 @@ monthprice[["decimal"]] = seq(2010.375, by = 1/12, length = nrow(monthprice))
 
 # examine dataframe - again
 str(monthprice)
-'data.frame':	155 obs. of  3 variables:
+'data.frame':	157 obs. of  3 variables:
  $ month  : Date, format: "2010-05-15" "2010-06-15" ...
  $ price  : num  17.6 17.4 18.1 18.4 20.2 ...
  $ decimal: num  2010 2010 2011 2011 2011 ...
@@ -73,19 +70,19 @@ str(monthprice)
 rawdata$week <- as.aweek(rawdata$date) 
 tail(rawdata$week)
 <aweek start: Monday>
-[1] "2023-W12-5" "2023-W13-1" "2023-W13-2" "2023-W13-3" "2023-W13-4"
-[6] "2023-W13-5" 
+[1] "2023-W18-2" "2023-W18-3" "2023-W18-4" "2023-W18-5" "2023-W19-1"
+[6] "2023-W19-2"
 
 # remove week day part from aweek week and stay in aweek format
 rawdata$week <- trunc(rawdata$week) 
 
 str(rawdata)
-'data.frame':	1569 obs. of  5 variables:
+'data.frame':	1593 obs. of  5 variables:
  $ date     : Date, format: "2010-05-14" "2010-05-21" ...
  $ price    : num  17.8 17.5 17.5 17 17.8 ...
  $ reference: chr  "http://www.carbonnews.co.nz/story.asp?storyID=4529" "http://www.carbonnews.co.nz/story.asp?storyID=4540" "http://www.carbonnews.co.nz/story.asp?storyID=4540" "http://www.carbonnews.co.nz/story.asp?storyID=4588" ...
- $ month    : Factor w/ 155 levels "2010-05","2010-06",..: 1 1 1 2 2 2 3 3 4 4 ...
- $ week     :Class 'aweek'  atomic [1:1569] 2010-W19-5 2010-W20-5 2010-W21-6 2010-W23-5 ...
+ $ month    : Factor w/ 157 levels "2010-05","2010-06",..: 1 1 1 2 2 2 3 3 4 4 ...
+ $ week     :Class 'aweek'  atomic [1:1593] 2010-W19-5 2010-W20-5 2010-W21-6 2010-W23-5 ...
   .. ..- attr(*, "week_start")= int 1 
   
 # create new dataframe of weekly mean price using 'aweek' variable 
@@ -95,8 +92,8 @@ weeklyprice <- aggregate(price ~ week, rawdata, mean)
 weeklyprice[["price"]] = round(weeklyprice[["price"]], digits = 2)
 
 str(weeklyprice)
-'data.frame':	578 obs. of  2 variables:
- $ week :Class 'aweek'  atomic [1:570] 2010-W19 2010-W20 2010-W21 2010-W23 ...
+'data.frame':	584 obs. of  2 variables:
+ $ week :Class 'aweek'  atomic [1:584] 2010-W19 2010-W20 2010-W21 2010-W23 ...
   .. ..- attr(*, "week_start")= int 1
  $ price: num  17.8 17.5 17.5 17 17.8 ...  
 
@@ -104,7 +101,7 @@ str(weeklyprice)
 weeklyprice[["date"]] <- as.Date(weeklyprice[["week"]])
 
 str(weeklyprice)
-'data.frame':	578 obs. of  3 variables:
+'data.frame':	584 obs. of  3 variables:
  $ week :Class 'aweek'  atomic [1:578] 2010-W19 2010-W20 2010-W21 2010-W23 ...
   .. ..- attr(*, "week_start")= int 1
  $ price: num  17.8 17.5 17.5 17 17.8 ...
@@ -165,7 +162,7 @@ dev.off()
 
 sessionInfo()
 R version 3.3.3 (2017-03-06)
-gdrive info 1jsYmImqbvM6WAT-o0IpeQ9mK9QV8JxZC
+Platform: i686-pc-linux-gnu (32-bit)
 Running under: Debian GNU/Linux 9 (stretch)
 
 locale:
@@ -174,13 +171,13 @@ locale:
  [5] LC_MONETARY=en_NZ.UTF-8       LC_MESSAGES=en_NZ.UTF-8      
  [7] LC_PAPER=en_NZ.UTF-8          LC_NAME=en_NZ.UTF-8          
  [9] LC_ADDRESS=en_NZ.UTF-8        LC_TELEPHONE=en_NZ.UTF-8     
-[11] LC_MEASUREMENT=en_NZ.UTF-8    LC_IDENTIFICATION=en_NZ.UTF-8
-
-attached base packages:
-[1] stats     graphics  grDevices utils     datasets  methods   base     
+[11] L  graphics  grDevices utils     datasets  methods   base     
 
 other attached packages:
-[1] rkward_0.6.5
+[1] aweek_1.0.3  rkward_0.6.5
 
 loaded via a namespace (and not attached):
-[1] tools_3.3.3
+[1] tools_3.3.3C_MEASUREMENT=en_NZ.UTF-8    LC_IDENTIFICATION=en_NZ.UTF-8
+
+attached base packages:
+[1] stats
